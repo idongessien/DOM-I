1f2806667eba3a5c633502a12c3ff35c06f90459
Idong Essien <idong.essien@comcast.net>
2020-02-10 19:44:04 -0500

    main content successful

--- js/index.js
+++ js/index.js
@@ -41,6 +41,13 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+const ctaImage = document.querySelector("#cta-img");
+ctaImage.setAttribute('src', siteContent["cta"]["img-src"])
+
+document.querySelector("h1").textContent = siteContent["cta"]["h1"];
+
+document.querySelector("button").textContent = siteContent["cta"]["button"];
+
 document.querySelector("a").textContent = siteContent["nav"]["nav-item-1"];
 document.querySelector("a:nth-child(2)").textContent = siteContent["nav"]["nav-item-2"];
 document.querySelector("a:nth-child(3)").textContent = siteContent["nav"]["nav-item-3"];
@@ -48,20 +55,20 @@ document.querySelector("a:nth-child(4)").textContent = siteContent["nav"]["nav-i
 document.querySelector("a:nth-child(5)").textContent = siteContent["nav"]["nav-item-5"];
 document.querySelector("a:nth-child(6)").textContent = siteContent["nav"]["nav-item-6"];
 
-
-
-
-
-
-
+document.querySelector("h4").textContent = siteContent["main-content"]["features-h4"];
+document.querySelector("p").textContent = siteContent["main-content"]["features-content"];
+document.querySelector(".text-content:nth-child(2) h4").textContent = siteContent["main-content"]["about-h4"];
+document.querySelector(".text-content:nth-child(2) p").textContent = siteContent["main-content"]["about-content"];
 
 var midPgImg = document.querySelector("#middle-img");
 midPgImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
-const ctaImage = document.querySelector("#cta-img");
-ctaImage.setAttribute('src', siteContent["cta"]["img-src"])
+document.querySelector(".text-content:nth-child(3) h4").textContent = siteContent["main-content"]["services-h4"];
+document.querySelector(".text-content:nth-child(3) p").textContent = siteContent["main-content"]["services-content"];
+document.querySelector(".bottom-content .text-content:nth-last-child(2) h4").textContent = siteContent["main-content"]["product-h4"];
+document.querySelector(".bottom-content .text-content:nth-last-child(2) p").textContent = siteContent["main-content"]["product-content"];
+document.querySelector(".bottom-content:last-child h4").textContent = siteContent["main-content"]["vision-h4"];
+document.querySelector(".bottom-content:last-child p").textContent = siteContent["main-content"]["vision-content"];
 
-document.querySelector("h1").textContent = siteContent["cta"]["h1"];
 
-document.querySelector("button").textContent = siteContent["cta"]["button"];
 
